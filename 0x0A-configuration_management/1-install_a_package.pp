6076ca9d7ddfff7193c6ca171c2f31f894c58371
Sibongile <sibongile.nhlema@gmail.com>
2024-02-24 20:53:51 +0200

    added a script to install werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 #!/usr/bin/pup
 # Installs flask from pip3, version 2.1.0
+package { 'Werkzeug':
+ensure  => '2.1.1',
+provider => 'pip3',
+}
+
 package { 'Flask':
 ensure   => '2.1.0',
 provider => 'pip3',
