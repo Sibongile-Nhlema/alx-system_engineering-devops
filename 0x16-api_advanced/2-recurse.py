494f790c2d79e2513c4e60afb709d8064e7f4e89
Sibongile <sibongile.nhlema@gmail.com>
2024-04-09 20:27:30 +0200

    adjusted the prototype

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
     script returns the list of titles of all hot
     articles for a given subreddit
@@ -15,19 +15,20 @@ def recurse(subreddit, hot_list=[]):
     if hot_list is None:
         hot_list = []
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
     params = {
-        "limit": 100
+        "limit": 100,
     }
-
+    
     if after:
         params['after'] = after
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
