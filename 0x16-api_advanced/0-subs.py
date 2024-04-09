94d5cf4026205b7a955fccbf15ce015db2da6312
Sibongile <sibongile.nhlema@gmail.com>
2024-04-09 12:44:39 +0200

    removed the try and except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,16 +11,12 @@ def number_of_subscribers(subreddit):
          -subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64;\
-               x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
-    try:
+    else:
         data = response.json()
         subscribers = data['data']['subscribers']
         return (subscribers)
-    except (KeyError, TypeError):
-        return 0
