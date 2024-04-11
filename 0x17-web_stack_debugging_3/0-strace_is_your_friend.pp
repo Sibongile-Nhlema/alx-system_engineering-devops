864062a92a94bced3e1d253d4d3fa700e912fd84
Sibongile <sibongile.nhlema@gmail.com>
2024-04-11 19:41:49 +0200

    simplified the script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-# Puppet manifest to fix `phpp` extensions to `php` in the WordPress file `wp-settings.php`
+# replace `phpp` file extensions with `php` in the WordPress file `wp-settings.php`
 
-$wordpress_file = '/var/www/html/wp-settings.php'
-
-# Use `sed` command to replace `.phpp` with `.php` in the WordPress file
 exec { 'fix-wordpress-phpp':
-  command => "sed -i 's/\\.phpp$/.php/' ${wordpress_file}",
-  path    => '/bin:/usr/bin',
-  onlyif  => "grep -q '\\.phpp$' ${wordpress_file}",
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
