00941e4717ee42f3bdb12f10fb3d097ce6ee2c47
Sibongile <sibongile.nhlema@gmail.com>
2024-04-11 19:35:10 +0200

    fixed alignment issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,7 +4,7 @@ $wordpress_file = '/var/www/html/wp-settings.php'
 
 # Use `sed` command to replace `.phpp` with `.php` in the WordPress file
 exec { 'fix-wordpress-phpp':
-  command     => "sed -i 's/\\.phpp$/.php/' ${wordpress_file}",
-  path        => '/bin:/usr/bin',
-  onlyif      => "grep -q '\\.phpp$' ${wordpress_file}",
+  command => "sed -i 's/\\.phpp$/.php/' ${wordpress_file}",
+  path    => '/bin:/usr/bin',
+  onlyif  => "grep -q '\\.phpp$' ${wordpress_file}",
 }
