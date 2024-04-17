94be089485feefc31b5e99340c032268ca849403
Sibongile <sibongile.nhlema@gmail.com>
2024-04-17 23:15:33 +0200

    automated the fixing of the ulimit

---
+++ 0x18-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fixing the failing requests issue on our web server - Ulimit config
+
+# update the ulimit values to accomodate the requests (soft - 1024, hard - 4096)
+exec { 'fix--ulimit--to--hard--limit':
+  command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin/',
+}
+
+#restart nginx
+exec { 'restart-nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => 'etc/init.d/',
+}
